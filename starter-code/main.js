de6fd5c7914e6dcb3caf798263bba10f8de78aba
imaninyasia <imani.fecu72@ymail.com>
2016-09-18 20:21:11 -0400

    created more java new

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,11 +3,21 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
-if (cardTwo === cardFour)
+/*if (cardTwo === cardFour)
 {
 	alert("You found a match!")
 }
 else
 {
 	alert("Sorry, try again.")
+}*/
+
+var x = document.getElementById('game-board');
+function creatBoard(){
+	for (var i =0; i<cards.length; i++)
+	{
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+	}
 }
